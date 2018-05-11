249d5e975698dcdac6e9d67581dc1e15a03d0b8f
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-05-11 00:32:59 -0400

    updated git ignore

--- public/app.js
+++ public/app.js
@@ -1,59 +1,69 @@
 
-
 $(document).ready(function () {
 
 
-// Click event to mark an article as saved
-$(document).on("click", "#saveButton", function() {
+  // Click event to mark an article as saved//NOT WORKING!
+  $(document).on("click", "#saveButton", function () {
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+      method: "POST",
+      url: "/saved/" + id,
+      data: {
+      }
+    })
+      .then(function (data) {
+
+        console.log(data);
+
+        location.reload();
+      });
+  });
+
+
+  //Click event to save note to database//NOT WORKING!!
+
+  $(document).on("click", "#submitNote", function () {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+
+    // Run a POST request to save the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/notes/" + thisId,
+      data: {
+
+        title: $("#titleInput").val(),
+        body: $("#bodyInput").val()
+      }
+    })
+      .then(function (data) {
+
+        console.log(data);
+      });
+
+  });
+
+})
+
+//Click event to remove article from saved articles list
+
+$(document).on("click", "#delButton", function () {
   var id = $(this).attr("data-id");
 
   $.ajax({
-    method: "POST",
-    url: "/saved/" + id,
+    method: "DELETE",
+    url: "/articles/" + id,
     data: {
     }
   })
-    .then(function(data) {
+    .then(function (data) {
 
       console.log(data);
 
       location.reload();
     });
 });
-
-
-
-//Click event to save note to database
-
-$(document).on("click", "#submitNote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/notes/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleInput").val(),
-      // Value taken from note textarea
-      body: $("#bodyInput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-    });
-
-});
-
-  })
-
-
-
-
-//Click event to remove article from saved articles list
 
 
 
--- server.js
+++ server.js
@@ -41,68 +41,63 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapeArticles
 mongoose.connect("mongodb://localhost/scrapeArticles");
 
 // Routes
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   db.Article
-  .find({})
-  .then(function(dbArticle) {
-    res.render('index', { articles: dbArticle } );
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
-});
-
-app.get("/saved", function(req, res) {
-  db.Article
-  .find({saved: true})
-  .then(function(dbArticle) {
-    res.render('saved', { articles: dbArticle } );
-  })
-  .catch(function(err) {
-    res.json(err);
-  })
-});
-
-app.get("/notes", function(req, res) {
-  db.Note
-  .find({})
-  .then(function(dbNote) {
-    res.render('notes', { notes: dbNote } );
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    .find({})
+    .then(function (dbArticle) {
+      res.render('index', { articles: dbArticle });
     })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-app.post("/saved", function(req, res) {
+app.get("/saved", function (req, res) {
   db.Article
-  .update({saved: true})
-  .then(function(dbArticle) {
-    res.render('saved', { articles: dbArticle } );
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+    .find({ saved: true })
+    .then(function (dbArticle) {
+      res.render('saved', { articles: dbArticle });
+    })
+    .catch(function (err) {
+      res.json(err);
+    })
+});
+
+app.get("/articles", function (req, res) {
+  db.Note
+    .find({})
+    .then(function (dbNote) {
+      res.render('notes', { notes: dbNote });
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/saved", function (req, res) {
+  db.Article
+    .update({ saved: true })
+    .then(function (dbArticle) {
+      res.render('saved', { articles: dbArticle });
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 //GET route for scraping the news section of the Washingtonian website
@@ -126,12 +121,12 @@ app.get("/scrape", function (req, res) {
       var link = $("h4").children().attr("href");
       var summary = $(this).children("div.wash-sidebar__item-deck").text();
 
-    // Save results in an object and push into the results array 
-    result.push({
-      title: title,
-      link: link,
-      summary: summary
-    });
+      // Save results in an object and push into the results array 
+      result.push({
+        title: title,
+        link: link,
+        summary: summary
+      });
 
 
       // Create a new Article using the `result` object built from scraping
@@ -149,7 +144,7 @@ app.get("/scrape", function (req, res) {
   })
 
   // Return to main page once finished scraping the text
-      res.redirect("/");
+  res.redirect("/");
 
 });
 
@@ -191,7 +186,7 @@ app.get("/scrape", function (req, res) {
 //   // Create a new note and pass the req.body to the entry
 //   db.Note.create(req.body)
 //     .then(function (dbNote) {
-  
+
 //       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
 //     })
 //     .then(function (dbArticle) {
